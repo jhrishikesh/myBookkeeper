f928b463b740345ee633174f8f6c61670840cb0b
hrishi <hrishi@hrishikeshs-air>
2017-12-03 20:52:48 +1100

    Initial commit

---
+++ myBookKeeper/src/main/java/com/mbk/myBookKeeper/MyBookKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.mbk.myBookKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookKeeperApplication.class, args);
+	}
+}
